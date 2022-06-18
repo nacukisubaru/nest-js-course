c9c662abde4be7d524d78d6a7047552e249f57be
nacukisubaru <alexitsheff@gmail.com>
2022-06-18 21:09:47 +0300

    связь many to many user и roles при помощи декораторов @BelongsToMany и @ForeignKey

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { User } from "./users/users.model";
 import { RolesController } from './roles/roles.controller';
 import { RolesModule } from './roles/roles.module';
 import { Role } from "./roles/roles.model";
+import { UserRoles } from "./roles/user-roles.model";
 
 @Module({
     controllers: [RolesController],
@@ -23,7 +24,7 @@ import { Role } from "./roles/roles.model";
             username: process.env.POSTGRES_USER,
             password: process.env.POSTGRES_PASSWORD,
             database: process.env.POSTGRES_DB,
-            models: [User, Role],
+            models: [User, Role, UserRoles],
             autoLoadModels: true
         }),
         UsersModule,
--- src/roles/roles.model.ts
+++ src/roles/roles.model.ts
@@ -1,5 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import {Column, DataType, Model, Table} from "sequelize-typescript"
+import {BelongsToMany, Column, DataType, Model, Table} from "sequelize-typescript"
+import { User } from "src/users/users.model";
+import { UserRoles } from "./user-roles.model";
 
 interface RoleCreationAttrs {
     value: string;
@@ -19,4 +21,7 @@ export class Role extends Model<Role, RoleCreationAttrs> {
     @ApiProperty({example: 'Админ', description: 'Описание роли'})
     @Column({type: DataType.STRING, allowNull: false})
     description: string;
+
+    @BelongsToMany(() => User, () => UserRoles)
+    users: User[];
 }
