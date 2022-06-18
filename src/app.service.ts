559dc49cd4cfb18e25ca7af264f349ae057eb9d1
nacukisubaru <alexitsheff@gmail.com>
2022-06-18 12:16:37 +0300

    users

--- src/app.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-//делаем класс инъектируемым
-@Injectable()
-export class AppService {
-    getUsers() {
-        return [{id: 1, name: 'Alex'}];
-    }
-}
