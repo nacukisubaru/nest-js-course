16e33476f813c7502b95dd11cedc1752142dfd32
nacukisubaru <alexitsheff@gmail.com>
2022-06-18 21:59:00 +0300

    инъектируем RolesService в UserService подключаем RolesModule в usermodule

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { RolesService } from 'src/roles/roles.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './users.model';
 
 @Injectable()
 export class UsersService {
     //иньектируем модель User в InjectModel
-    constructor(@InjectModel(User) private userRepository: typeof User) {}
+    constructor(@InjectModel(User) private userRepository: typeof User,
+        private rolesService: RolesService) {}
  
     async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
