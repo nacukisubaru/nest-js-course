caf6d4be7b2a482d75aa4285db3545d2d47b959f
nacukisubaru <alexitsheff@gmail.com>
2022-06-18 12:19:27 +0300

    создание системных переменых из файла .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
-import { UsersModule } from './users/users.module';
 
+import { UsersModule } from './users/users.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from "@nestjs/config";
 @Module({
-    controllers: [AppController],
-    providers: [AppService],
-    imports: [UsersModule]
+    controllers: [],
+    providers: [],
+    imports: [
+        //создает системные переменные из файла
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'ten9285ten',
+            database: 'nest-course',
+            models: [],
+            autoLoadModels: true
+        }),
+        UsersModule
+    ]
 })
 export class AppModule {}
