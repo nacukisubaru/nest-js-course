b82eb5d4cba8100fb72ae6e31deea957c0cfe474
nacukisubaru <alexitsheff@gmail.com>
2022-06-19 12:04:42 +0300

    авторизация через jwt токен

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
@@ -11,7 +11,8 @@ export class AuthService {
                 private jwtService: JwtService) {}
 
     async login( userDto: CreateUserDto) {
-       
+        const user = await this.validateUser(userDto);
+        return this.genereateToken(user);
     }
 
     async registration(userDto: CreateUserDto) {
@@ -30,6 +31,16 @@ export class AuthService {
         return {
             token: this.jwtService.sign(payload)
         }
+    }
+
+    private async validateUser(userDto: CreateUserDto) {
+        const user = await this.userService.getUserByEmail(userDto.email);
+        const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+        if(user && passwordEquals) {
+            return user;
+        }
+
+        throw new UnauthorizedException({message: 'Некорректный email или пароль'})
     }
 
 }
