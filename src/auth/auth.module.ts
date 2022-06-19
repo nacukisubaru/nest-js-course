3f0f20b160b8fc14cbd753d9baa2d7c3197142f6
nacukisubaru <alexitsheff@gmail.com>
2022-06-19 11:13:06 +0300

    подключаение UserModule в auth.module для использования UserService

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+    providers: [AuthService],
+    controllers: [AuthController],
+    imports: [
+        UsersModule
+    ]
 })
-export class AuthModule {}
+export class AuthModule { }
