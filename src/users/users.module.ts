0ba05008787fd642bbd3de05ff3c6fef7e047e23
nacukisubaru <alexitsheff@gmail.com>
2022-06-21 18:54:09 +0300

    Guards не даем получить список пользователей если не авторизованы + устранение кольцевой зависимости

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -6,12 +6,17 @@ import { User } from './users.model';
 import { Role } from 'src/roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 @Module({
     controllers: [UsersController],
     providers: [UsersService],
     imports: [
         SequelizeModule.forFeature([User, Role, UserRoles]),
-        RolesModule
+        RolesModule,
+        //для устранения кольцевой зависимости
+        //так как мы используем модуль авторизации в пользователях
+        // а пользователей в авторизации
+        forwardRef(() => AuthModule)
     ],
     exports: [UsersService]
 })
