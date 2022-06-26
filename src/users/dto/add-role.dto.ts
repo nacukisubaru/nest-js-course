93f46bf5f987630f22bae8cd7dc23236c34e31be
nacukisubaru <alexitsheff@gmail.com>
2022-06-26 12:36:07 +0300

    валидация роли dto

--- src/users/dto/add-role.dto.ts
+++ src/users/dto/add-role.dto.ts
@@ -1,9 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString } from "class-validator";
 
 export class AddRoleDto {
 
     @ApiProperty({example: '5', description: 'id пользователя'})
+    @IsNumber({}, {message:"Должно быть числом"})
     readonly userId: number;
+
     @ApiProperty({example: '1-ADMIN, 2-USER', description: 'Роль'})
+    @IsString({message: "Должно быть строкой"})
     readonly value: string;
 }
